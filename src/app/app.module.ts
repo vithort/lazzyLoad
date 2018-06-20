be70caed42a16b1f11aa320e13adfea27e701225
vithort <vithort@gmail.com>
2018-06-20 14:35:09 -0300

    Seção 8 Aula 28 Usando carregamento lazy de páginas
    .. criar o projeto:
    > ionic start lazzyLoad blank
    .. criar página de cadastro
    > ionic generate page cadastrar
    .. não vai importar no app.module.ts, ja que essa é a pagian que abre quando inicia o app, as paginas vao ser importadas quando fore mutilizadas
    .. colocar o exports no cadastrar.module.ts
    .. criar metodo e botao no home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,22 +9,25 @@ import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    MyApp
+    ,HomePage
+  ]
+  ,imports: [
+    BrowserModule
+    ,IonicModule.forRoot(MyApp)
+  ]
+  ,bootstrap: [IonicApp]
+  ,entryComponents: [
+    MyApp
+    ,HomePage
+  ]
+  ,providers: [
+    StatusBar
+    ,SplashScreen
+    ,{
+      provide: ErrorHandler
+      ,useClass: IonicErrorHandler
+    }
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { CadastrarPage } from '../cadastrar/cadastrar';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-home'
+  ,templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController
+  ) {
 
+  }
+
+  cadastrar() {
+    // desse modo funciona, porem se voce refatorar / mudar o nome da pagina, ele precisara ser alterado manualmente (nao precisa do import da Page):
+    //this.navCtrl.push('CadastrarPage');
+    // desse outro modo ele vai buscar o nome da pagina:
+    this.navCtrl.push(CadastrarPage.name);
   }
 
 }
